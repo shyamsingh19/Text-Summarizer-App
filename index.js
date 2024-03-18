b3ee523d479fe05b021a44e15d9a19b3b45ca1f2
shyam sunder singh <shyamsunders507@gmail.com>
2024-03-18 20:36:05 +0530

    port update

--- index.js
+++ index.js
@@ -29,5 +29,5 @@ app.post('/summarize', (req, res) => {
 
 // Start the server
 app.listen(port, () => {
-  console.log('Server running at http://localhost:${port}/');
+  console.log(`Server running at http://localhost:${port}/`);
 });
